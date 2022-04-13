36d543609f67b4a0fe30d0190220c0cf7501689b
Tomilov Sergey <tomexpert@yandex.ru>
2022-04-13 17:16:14 +0800

    fix router

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # REACT
-### [Итоговая работа - интернет-магазин обуви](https://github.com/TomSG03/ra-diplom)
-### [Gh-Pages]()
+### [Итоговая работа - интернет-магазин обуви](https://github.com/TomSG03/ra16-diploma)
+### [Gh-Pages](https://tomsg03.github.io/ra-diplom/)
 ### [Back-End](https://github.com/TomSG03/ra-diplom-be)
 <!-- ### [Heroky](https://git.heroku.com/ahj-diplom-be.git) -->
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Layout from './components/LayOut';
 import Home from './components/Home';
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Layout>
           <Routes>
             <Route path='/' element={<Home />} />
@@ -24,7 +24,7 @@ function App() {
             <Route path="*" element={<Page404 />} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
