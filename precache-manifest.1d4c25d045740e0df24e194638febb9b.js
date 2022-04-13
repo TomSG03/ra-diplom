a99bec3d45a603831ddf8e01b78c79879d063280
Tomilov Sergey <tomexpert@yandex.ru>
2022-04-13 17:07:09 +0800

    Updates

---
+++ precache-manifest.1d4c25d045740e0df24e194638febb9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106d31939ba95ba41baef5a974fd62ee",
+    "url": "/ra-diplom/index.html"
+  },
+  {
+    "revision": "8c65e64a0c5a30895412",
+    "url": "/ra-diplom/static/css/main.54e4c48c.chunk.css"
+  },
+  {
+    "revision": "9cabb57177f04e264955",
+    "url": "/ra-diplom/static/js/2.31716fc5.chunk.js"
+  },
+  {
+    "revision": "8c65e64a0c5a30895412",
+    "url": "/ra-diplom/static/js/main.cc14d5a7.chunk.js"
+  },
+  {
+    "revision": "d2edb09691cd509dd539",
+    "url": "/ra-diplom/static/js/runtime-main.7be8eb03.js"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/ra-diplom/static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "/ra-diplom/static/media/footer-sprite.9c087d43.png"
+  }
+]);
