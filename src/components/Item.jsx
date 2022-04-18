85d02d4719d587956da4e157f8535932e94f42b8
Tomilov Sergey <tomexpert@yandex.ru>
2022-04-18 15:12:23 +0800

    product count

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,12 +3,21 @@ import React, { useState } from 'react'
 
 function Item({ item }) {
   const [size, setSize] = useState(null);
+  const [count, setCount] = useState(0);
 
-  const handlerClick = ({ target }) => {
+  const handlerClickSize = ({ target }) => {
     setSize(Number(target.dataset.id))
   }
 
-  const sizes = item.sizes.map((e, index) => e.avalible ? <span className={size === index ? "catalog-item-size selected" : "catalog-item-size"} key={index} data-id={index} onClick={handlerClick}>{e.size}</span> : '')
+  const handlerClickCount = (e) => {
+    const num = Number(e.target.name);
+    let sizeCopy = count + num;
+    if (sizeCopy > 10) sizeCopy = 10; 
+    if (sizeCopy < 0) sizeCopy = 0; 
+    setCount(sizeCopy)
+  }
+
+  const sizes = item.sizes.map((e, index) => e.avalible ? <span className={size === index ? "catalog-item-size selected" : "catalog-item-size"} key={index} data-id={index} onClick={handlerClickSize}>{e.size}</span> : '')
 
   return (
     <section className="catalog-item">
@@ -49,13 +58,13 @@ function Item({ item }) {
           <div className="text-center">
             <p>Размеры в наличии: {sizes}</p>
             <p>Количество: <span className="btn-group btn-group-sm pl-2">
-              <button className="btn btn-secondary">-</button>
-              <span className="btn btn-outline-primary">1</span>
-              <button className="btn btn-secondary">+</button>
+              <button className="btn btn-secondary" onClick={handlerClickCount} name='-1'>-</button>
+              <span className="btn btn-outline-primary">{count}</span>
+              <button className="btn btn-secondary" onClick={handlerClickCount} name='+1'>+</button>
             </span>
             </p>
           </div>
-          <button className="btn btn-danger btn-block btn-lg" >В корзину</button>
+          <button className="btn btn-danger btn-block btn-lg" disabled={(count > 0) && (size !== null) ? false : true}>В корзину</button>
         </div>
       </div>
     </section>
